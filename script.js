21b8f289493da996facfd18f0118d7e9584a2c8f
tventura94 <71479822+tventura94@users.noreply.github.com>
2023-01-24 17:27:27 -0500

    deleted logs

--- script.js
+++ script.js
@@ -40,17 +40,6 @@ document.addEventListener('keydown', function (e) {
 // Button scrolling
 btnScrollTo.addEventListener('click', function (e) {
   const s1coords = section1.getBoundingClientRect();
-  console.log(s1coords);
-
-  console.log(e.target.getBoundingClientRect());
-
-  console.log('Current scroll (X/Y)', window.pageXOffset, window.pageYOffset);
-
-  console.log(
-    'height/width viewport',
-    document.documentElement.clientHeight,
-    document.documentElement.clientWidth
-  );
 
   section1.scrollIntoView({ behavior: 'smooth' });
 });
